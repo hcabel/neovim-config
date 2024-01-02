5a7c35f007192e6e9438181b92c54d9ccf2f239b
hcabel <hcabel@student.42.fr>
2024-01-02 21:25:16 +1000

    More harpoon shortcut <leader>1 to <leader>9

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -1,3 +1,5 @@
+local vim = vim; -- silence linter
+
 local harpoon = require("harpoon")
 
 -- Calculate the size of the whole Neovim window
@@ -25,4 +27,9 @@ vim.keymap.set('n', "<leader>1", function () ui.nav_file(1) end)
 vim.keymap.set('n', "<leader>2", function () ui.nav_file(2) end)
 vim.keymap.set('n', "<leader>3", function () ui.nav_file(3) end)
 vim.keymap.set('n', "<leader>4", function () ui.nav_file(4) end)
+vim.keymap.set('n', "<leader>5", function () ui.nav_file(5) end)
+vim.keymap.set('n', "<leader>6", function () ui.nav_file(6) end)
+vim.keymap.set('n', "<leader>7", function () ui.nav_file(7) end)
+vim.keymap.set('n', "<leader>8", function () ui.nav_file(8) end)
+vim.keymap.set('n', "<leader>9", function () ui.nav_file(9) end)
 
