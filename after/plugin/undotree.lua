c380b7575fa9b57b33838045351edfaf5074f351
hcabel <coding@hugocabel.com>
2024-11-09 09:55:35 +1000

    use diff/FC diff cmd based on OS

--- after/plugin/undotree.lua
+++ after/plugin/undotree.lua
@@ -1,3 +1,9 @@
 
-vim.g.undotree_DiffCommand = "FC"
+if vim.loop.os_uname().sysname == "Windows" then
+	vim.opt.undodir = os.getenv("UserProfile") .. "/.vim/undodir"
+	vim.g.undotree_DiffCommand = "FC"
+else
+	vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+	vim.g.undotree_DiffCommand = "diff"
+end
 vim.keymap.set('n', "<leader>u", vim.cmd.UndotreeToggle)
