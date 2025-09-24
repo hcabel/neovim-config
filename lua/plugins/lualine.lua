f1f40810160f53ef7c2c91412203be591b67da94
hcabel <coding@hugocabel.com>
2025-09-24 22:13:47 +0200

    Remove Noice statusline for lualine (duplicate)

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -94,13 +94,6 @@ return {
                     },
                 }
             },
-            lualine_x = {
-                {
-                    require("noice").api.statusline.mode.get,
-                    cond = require("noice").api.statusline.mode.has,
-                    color = { fg = "#ff9e64" },
-                }
-            },
             lualine_y = { "tabs" },
             lualine_z = { 'location' }
         },
