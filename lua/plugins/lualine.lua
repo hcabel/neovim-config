cc9a0d4325bc8f717c4a6a60d7b47896784df6ba
Hugo Cabel <coding@hugocabel.com>
2025-09-28 12:15:26 +0200

    Update lualine to work better with Noice

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,113 +1,95 @@
--- local theme_auto = require('lualine.themes.auto')
--- local bg_color = '#26233a'
--- theme_auto.normal.c.bg = bg_color
--- theme_auto.insert.c.bg = bg_color
--- theme_auto.visual.c.bg = bg_color
--- theme_auto.replace.c.bg = bg_color
--- theme_auto.command.c.bg = bg_color
--- theme_auto.inactive.c.bg = "#1c1a2a"
-
 return {
     'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons', opt = true },
-    opts = {
-        options = {
-            icons_enabled = true,
-            theme = "rose-pine",
-            section_separators = { left = '', right = '' },
-            component_separators = { left = '❱', right = '❰' },
-            disabled_filetypes = {
-                statusline = {},
-                winbar = {},
+    events = "VeryLazy",
+    opts = function()
+        local LazyVim = require("lazyvim.util")
+        -- PERF: we don't need this lualine require madness 🤷
+        local lualine_require = require("lualine_require")
+        lualine_require.require = require
+
+        local icons = LazyVim.config.icons
+
+        vim.o.laststatus = vim.g.lualine_laststatus
+        return {
+            options = {
+                theme = "auto",
+                globalstatus = vim.o.laststatus == 3,
+                section_separators = { left = '', right = '' },
+                component_separators = { left = '❱', right = '❰' },
+                disabled_filetypes = { statusline = { "dashboard", "alpha", "ministarter", "snacks_dashboard" } },
             },
-            ignore_focus = {},
-            always_divide_middle = true,
-            globalstatus = false,
-            refresh = {
-                statusline = 1000,
-                tabline = 1000,
-                winbar = 1000,
-            }
-        },
-        sections = {
-            lualine_a = { function() -- Custom Mode module to show recording status
-                local reg = vim.fn.reg_recording()
-                if reg ~= "" then
-                    return "Recording @" .. reg
-                else
-                    local mode = vim.api.nvim_get_mode().mode
-                    local mode_map = {
-                        n = 'NORMAL',
-                        i = 'INSERT',
-                        v = 'VISUAL',
-                        V = 'V-LINE',
-                        ['^V'] = 'V-BLOCK',
-                        c = 'COMMAND',
-                        R = 'REPLACE',
-                        s = 'SELECT',
-                        S = 'S-LINE',
-                        ['^S'] = 'S-BLOCK',
-                        t = 'TERMINAL',
-                    }
-                    return mode_map[mode] or mode:upper()
-                end
-            end },
-            lualine_b = {
-                'branch',
-                {
-                    'diff',
-                    symbols = {
-                        added = ' ',
-                        modified = ' ',
-                        removed = ' '
-                    }
-                }
-            },
-            lualine_c = {
-                {
-                    'filetype',
-                    icon_only = true,
-                    separator = '',
-                },
-                {
-                    padding = { left = 0, right = 1 },
-                    'filename',
-                    path = 1,
-                    symbols = {
-                        modified = '',
-                        readOnly = '',
-                        unamed = '',
-                        newfile = '',
+            sections = {
+                lualine_a = { function() -- Custom Mode module to show recording status
+                    local reg = vim.fn.reg_recording()
+                    if reg ~= "" then
+                        return "Recording @" .. reg
+                    else
+                        local mode = vim.api.nvim_get_mode().mode
+                        local mode_map = {
+                            n = 'NORMAL',
+                            i = 'INSERT',
+                            v = 'VISUAL',
+                            V = 'V-LINE',
+                            ['^V'] = 'V-BLOCK',
+                            c = 'COMMAND',
+                            R = 'REPLACE',
+                            s = 'SELECT',
+                            S = 'S-LINE',
+                            ['^S'] = 'S-BLOCK',
+                            t = 'TERMINAL',
+                        }
+                        return mode_map[mode] or mode:upper()
+                    end
+                end },
+                lualine_b = {
+                    'branch',
+                    {
+                        "diff",
+                        symbols = {
+                            added = icons.git.added,
+                            modified = icons.git.modified,
+                            removed = icons.git.removed,
+                        },
+                        source = function()
+                            local gitsigns = vim.b.gitsigns_status_dict
+                            if gitsigns then
+                                return {
+                                    added = gitsigns.added,
+                                    modified = gitsigns.changed,
+                                    removed = gitsigns.removed,
+                                }
+                            end
+                        end,
                     },
                 },
-                {
-                    'location',
-                    padding = { left = 0, right = 1 }
-                },
-                {
-                    'diagnostics',
-                    symbols = {
-                        error = ' ',
-                        warn = ' ',
-                        hint = ' ',
-                        info = ' '
+                lualine_c = {
+                    {
+                        'filetype',
+                        icon_only = true,
+                        separator = '',
                     },
-                }
+                    {
+                        padding = { left = 0, right = 1 },
+                        LazyVim.lualine.pretty_path()
+                    },
+                    {
+                        'location',
+                        padding = { left = 0, right = 1 }
+                    },
+                    {
+                        'diagnostics',
+                        symbols = {
+                            error = icons.diagnostics.Error,
+                            warn = icons.diagnostics.Warn,
+                            info = icons.diagnostics.Info,
+                            hint = icons.diagnostics.Hint,
+                        },
+                    }
+                },
+                lualine_y = {}, -- Override default lualine_y to be empty
+                lualine_z = { "progress" }
             },
-            lualine_y = { "tabs" },
-            lualine_z = { 'location' }
-        },
-        inactive_sections = {
-            lualine_a = {},
-            lualine_b = {},
-            lualine_c = {},
-            lualine_x = {},
-            lualine_y = {},
-            lualine_z = {}
-        },
-        tabline = {},
-        winbar = {},
-        inactive_winbar = {},
-        extensions = {}
-    }
+        }
+    end
 }
